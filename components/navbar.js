6196fd0dd1a09a3084c5be15ce53dfd3dc4dbf7d
benni347 <cdrc+gitkraken@skwar.me>
2022-01-26 22:29:50 +0100

    Added a border radius to the navbar buttons, so they have rounded corners.

--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,18 @@
 import Logo from './logo'
 import NextLink from 'next/link'
 import {
-  Container,
-  Box,
-  Link,
-  Stack,
-  Heading,
-  Flex,
-  Menu,
-  MenuItem,
-  MenuList,
-  MenuButton,
-  IconButton,
-  useColorModeValue
+    Container,
+    Box,
+    Link,
+    Stack,
+    Heading,
+    Flex,
+    Menu,
+    MenuItem,
+    MenuList,
+    MenuButton,
+    IconButton,
+    useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
@@ -29,6 +29,7 @@ const LinkItem = ({ href, path, children, _target, ...props }) => {
           color={active ? '#202023' : inactiveColor}
           _target={_target}
           {...props}
+          borderRadius="lg"
         >
           {children}
         </Link>
@@ -61,10 +62,10 @@ const NavBar = props => {
                 display={{ base: 'none', md: 'flex' }}
                 width={{ base: 'full', md: 'auto' }}
                 alignItems="center"
-                flexGrow={1}
                 mt={{ base: 4, md: 0 }}
+                flexGrow={1}
                 >
-                <LinkItem href="/works" path={path}>
+                <LinkItem href="/works" path={path} borderRadius="lg">
                 Works
                 </LinkItem>
                 <LinkItem href="/posts" path={path}>
