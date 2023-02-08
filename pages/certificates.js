b0c44b05027fbf038ce3ee5e0f70e2bb42b4f76d
Cédric Skwar <cdrc@skwar.me>
2023-02-08 16:30:53 +0100

    Changed the WIP certs to a list from a grid, because then I don't need to specify a thumbnail.

--- pages/certificates.js
+++ pages/certificates.js
@@ -4,6 +4,8 @@ import {
     Heading,
     Icon,
     Link,
+    List,
+    ListItem,
     SimpleGrid
 } from "@chakra-ui/react";
 import Section from "../components/section";
@@ -13,6 +15,7 @@ import { IoLogoPython } from "react-icons/io5";
 import certLpiEssential from "../public/images/certificates/lpi_essential.png";
 import certJsAlgorithmsDataStructures from "../public/images/certificates/javascript_algorithms_data_structure_fcc.png";
 import certScientificComputingPython from "../public/images/certificates/scientific_computing_python_fcc.png";
+import { Meta } from "../components/cert";
 
 const Page = () => {
     return (
@@ -59,22 +62,19 @@ const Page = () => {
                 <Heading as="h5" fontSize={16} mb={4}>
                     Work in progress
                 </Heading>
-                <SimpleGrid columns={[1, 2, 2]} gap={6}>
-                    <GridItem id="pcep-30">
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>
+                            <Icon as={IoLogoPython} />
+                        </Meta>
                         <Link
-                            href="https://pythoninstitute.org/pcep"
                             target="_blank"
+                            href="https://pythoninstitute.org/pcep"
                         >
-                            <Button
-                                variant="ghost"
-                                colorScheme="teal"
-                                leftIcon={<Icon as={IoLogoPython} />}
-                            >
-                                PCEP-30
-                            </Button>
+                            PCEP-30
                         </Link>
-                    </GridItem>
-                </SimpleGrid>
+                    </ListItem>
+                </List>
             </Container>
         </Layout>
     );
