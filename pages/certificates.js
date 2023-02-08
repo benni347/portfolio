618047fa378ecba30c6701e0d91cad920a7f8a11
Cédric Skwar <cdrc@skwar.me>
2023-02-08 16:23:34 +0100

    Added my newest js cert

--- pages/certificates.js
+++ pages/certificates.js
@@ -4,6 +4,7 @@ import Layout from '../components/layouts/article'
 import {GridItem} from '../components/grid-item'
 import {IoLogoPython} from 'react-icons/io5'
 import certLpiEssential from "../public/images/certificates/lpi_essential.png";
+import certJsAlgorithmsDataStructures from "../public/images/certificates/javascript_algorithms_data_structure_fcc.png";
 import certScientificComputingPython from "../public/images/certificates/scientific_computing_python_fcc.png";
 
 
@@ -38,17 +39,30 @@ const Page = () => {
                             {/*:TODO: Add an option to download the image and add a description to every cert.*/}
                         </GridItem>
                     </Section>
+                    <Section>
+                        <GridItem
+                            id="JsAlgorithmsDataStructures-Cert"
+                            title="JavaScript Algorithms and Data Structures Cert"
+                            thumbnail={certJsAlgorithmsDataStructures}
+                            href="./certificates/jsAlgorithmsDataStructures"
+                        >
+                            {/*:TODO: Add an option to download the image and add a description to every cert.*/}
+                        </GridItem>
+                    </Section>
                 </SimpleGrid>
                 <Heading as="h5" fontSize={16} mb={4}>
                     Work in progress
                 </Heading>
                 <SimpleGrid columns={[1, 2, 2]} gap={6}>
                     <GridItem id="pcep-30">
+
                         <Link href="https://pythoninstitute.org/pcep" target="_blank">
                             <Button variant="ghost" colorScheme="teal" leftIcon={<Icon as={IoLogoPython}/>}>
                                 PCEP-30
                             </Button>
+
                         </Link>
+
                     </GridItem>
                 </SimpleGrid>
             </Container>
---
+++ pages/certificates/jsAlgorithmsDataStructures.js
@@ -0,0 +1,30 @@
+import {Badge, Container, List, ListItem} from "@chakra-ui/react";
+import {Meta, Title} from "../../components/cert";
+import P from "../../components/paragraph";
+import Layout from "../../components/layouts/article";
+
+const Post = () => {
+    return (
+        <Layout title="Certificates">
+            <Container>
+                <Title>
+                    JS Algorithms and Data Structures <Badge>February/2023</Badge>
+                </Title>
+                <P>
+                    The JavaScript Algorithms and Data Structures required me to program four simple programs and one hard one.
+                    The hard one was the last one. The thing I found difficult about it was understanding the exercise.
+                </P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Websites</Meta>
+                        <a href="https://www.freecodecamp.org/certification/benni347/javascript-algorithms-and-data-structures">
+                            Certificate Link + Overview
+                        </a>
+                    </ListItem>
+                </List>
+            </Container>
+        </Layout>
+    );
+};
+
+export default Post;
