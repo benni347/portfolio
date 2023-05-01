de758ac22c984ae49fcd0bf551f3efc5e38584a4
Cedric Skwar <cdrc@skwar.me>
2023-05-01 06:57:04 +0200

    updated the text.

--- pages/posts/abschlussarbeit_update_2.js
+++ pages/posts/abschlussarbeit_update_2.js
@@ -11,37 +11,29 @@ const Post = () => {
                     Final Project Update 2 <Badge>2023-04-30</Badge>
                 </Title>
                 <P>
-                    Currently, I am in the process of devising my final project,
-                    utilizing the exclusive design suite known as figma. I have
-                    had the pleasure of reviewing a web development tutorial
-                    focused on figma, which I have included a link to below. It
-                    proved to be quite enlightening and informative; therefore,
-                    I highly recommend it. As for my color palette for the
-                    messenger, I have decided on the following hues:
-                    <List>
-                        <ListItem>
-                            primary color - a deep shade of blue (#1B2C4D)
-                        </ListItem>
-                        <ListItem>
-                            secondary color - a light blue tone (#5C80BC)
-                        </ListItem>
-                        <ListItem>accent color - green (#21B372)</ListItem>
-                        <ListItem>
-                            neutral color 1 - a light grey tint (#F2F2F2)
-                        </ListItem>
-                        <ListItem>
-                            neutral color 2 - a dark grey hue (#2D2D2D).
-                        </ListItem>
-                    </List>{" "}
+                    At present, I am contemplating the transition of my network
+                    stack from WebSockets to WebRTC. This consideration arises
+                    due to the numerous glitches and anomalies encountered
+                    during message transmission, yet their origins remain
+                    elusive despite my efforts to employ print statements
+                    ubiquitously, which have proven futile.
+                </P>
+                <br></br>
+                <P>
+                    Utilizing WebRTC data channels would facilitate message
+                    transmission, while simultaneously enabling the effortless
+                    incorporation of calling capabilities. This supposition is
+                    grounded in the frequent employment of WebRTC for video and
+                    audio communication applications.
                 </P>
                 <List ml={4} my={4}>
                     <ListItem>
                         <Meta>Programminglanguages</Meta>
-                        <span>Figma </span>
+                        <span>Go, JS</span>
                     </ListItem>
                     <ListItem>
                         <Meta>DATE</Meta>
-                        <span>2023-03-30</span>
+                        <span>2023-04-30</span>
                     </ListItem>
                 </List>
             </Container>
