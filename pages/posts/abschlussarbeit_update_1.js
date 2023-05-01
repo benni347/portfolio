6afad01e62d6a45ac2e018fff31b49065506d8a0
Cedric Skwar <cdrc@skwar.me>
2023-05-01 06:30:45 +0200

    Linted the update 1.

--- pages/posts/abschlussarbeit_update_1.js
+++ pages/posts/abschlussarbeit_update_1.js
@@ -3,7 +3,6 @@ import { Meta, Title } from "../../components/posts";
 import P from "../../components/paragraph";
 import Layout from "../../components/layouts/article";
 
-
 const Post = () => {
     return (
         <Layout title="Final Project">
@@ -12,17 +11,43 @@ const Post = () => {
                     Final Project Update 1 <Badge>2023</Badge>
                 </Title>
                 <P>
-Currently, I am in the process of devising my final project, utilizing the exclusive design suite known as figma. I have had the pleasure of reviewing a web development tutorial focused on figma, which I have included a link to below. It proved to be quite enlightening and informative; therefore, I highly recommend it. As for my color palette for the messenger, I have decided on the following hues:
-        <List><ListItem>primary color - a deep shade of blue (#1B2C4D)</ListItem><ListItem>secondary color - a light blue tone (#5C80BC)</ListItem><ListItem>accent color - green (#21B372)</ListItem><ListItem>neutral color 1 - a light grey tint (#F2F2F2)</ListItem><ListItem>neutral color 2 - a dark grey hue (#2D2D2D).</ListItem></List>        </P>
+                    Currently, I am in the process of devising my final project,
+                    utilizing the exclusive design suite known as figma. I have
+                    had the pleasure of reviewing a web development tutorial
+                    focused on figma, which I have included a link to below. It
+                    proved to be quite enlightening and informative; therefore,
+                    I highly recommend it. As for my color palette for the
+                    messenger, I have decided on the following hues:
+                    <List>
+                        <ListItem>
+                            primary color - a deep shade of blue (#1B2C4D)
+                        </ListItem>
+                        <ListItem>
+                            secondary color - a light blue tone (#5C80BC)
+                        </ListItem>
+                        <ListItem>accent color - green (#21B372)</ListItem>
+                        <ListItem>
+                            neutral color 1 - a light grey tint (#F2F2F2)
+                        </ListItem>
+                        <ListItem>
+                            neutral color 2 - a dark grey hue (#2D2D2D).
+                        </ListItem>
+                    </List>{" "}
+                </P>
                 <List ml={4} my={4}>
                     <ListItem>
                         <Meta>Figma</Meta>
-                        <a href="https://www.figma.com/">
-                            Figma Homepage                        </a>
+                        <a href="https://www.figma.com/">Figma Homepage </a>
                     </ListItem>
-        <ListItem>
-        <Meta>Tutorial</Meta>
-                        <a href="https://www.youtube.com/watch?v=2liZ3uvO9bs" target="_blank" rel="noreferrer">Youtube for Tutorial</a>
+                    <ListItem>
+                        <Meta>Tutorial</Meta>
+                        <a
+                            href="https://www.youtube.com/watch?v=2liZ3uvO9bs"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Youtube for Tutorial
+                        </a>
                     </ListItem>
                     <ListItem>
                         <Meta>Programminglanguages</Meta>
