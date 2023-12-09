817e1a167c35814e091eca94f5829aebf7b5af7b
Cedric Skwar <cdrc@5y5.one>
2023-12-09 12:08:32 +0100

    health check

---
+++ pages/api/health.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const healthInfo = {
+    status: 'ok',
+    description: 'Cedrics Portfolio',
+    uptime: process.uptime(), // Uptime in seconds
+  };
+
+  res.status(200).json(healthInfo);
+}
